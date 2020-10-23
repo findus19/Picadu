4e4502727bf4655337e88b5f3afea980307554f6
findus19 <ivanexs@mail.ru>
2020-10-23 20:36:06 +0300

    Picadu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector('#menu-toggle'),
+    menu = document.querySelector('.sidebar');
+
+
+menuToggle.addEventListener('click', e => {
+    e.preventDefault();
+    menu.classList.toggle('visible');
+});
